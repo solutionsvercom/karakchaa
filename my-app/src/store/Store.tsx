e24396018cc9fc161dfcd11e8947a940e3c897ac
Rishav Parashar <rishavparashar4635@gmail.com>
2026-02-16 12:24:45 +0530

    empolyee

--- my-app/src/store/Store.tsx
+++ my-app/src/store/Store.tsx
@@ -1,27 +1,20 @@
+
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "../features/CustomersSlice";
-<<<<<<< HEAD
+// import productReducer from "../features/ProductsSlice";
 import stockReducer from "../features/StockmanagementSlice";
+// import salesReducer from "../features/SalesSlice";
 import employeesReducer from "../features/EmployeesSlice";
 import authReducer from "../features/AuthSlice";
-export const store = configureStore({
-  reducer: {
-     employees: employeesReducer,
-    customer: customerReducer,
-    stock: stockReducer,
-    auth: authReducer,
-=======
-import productReducer from "../features/ProductsSlice";
-import salesReducer from "../features/SalesSlice";
-import employeesReducer from "../features/EmployeesSlice";
 
 export const store = configureStore({
   reducer: {
     customer: customerReducer,
-    product : productReducer,
-    sales : salesReducer,
+    auth: authReducer,
+    stock: stockReducer,
+    // product : productReducer,
+    // sales : salesReducer,
     employees: employeesReducer,
->>>>>>> e279d8c36ae24903da95d253960efaa0d52b1310
   },
 });
 
