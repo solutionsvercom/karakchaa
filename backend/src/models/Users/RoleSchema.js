d50ab7d58cc8aa9a9d4fbecbbf3a80bcc409b2b4
Rishav Parashar <rishavparashar4635@gmail.com>
2026-02-16 20:15:43 +0530

    Users and Role Modules Backend

---
+++ backend/src/models/Users/RoleSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    modules: [{
+        type: String
+    }],
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Role", roleSchema);
